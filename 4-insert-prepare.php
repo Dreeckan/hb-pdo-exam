5a0df64b637db7e4af46471f5c48d8ae37e7c0dd
franckcode6 <franck.quasevi@yahoo.com>
2021-02-01 15:03:36 +0100

    Update 4-insert-prepare.php

--- 4-insert-prepare.php
+++ 4-insert-prepare.php
@@ -23,5 +23,5 @@ foreach ($data as $beanie) {
         
     $id = $connection->lastInsertId();
         
-    echo 'Erreur '.$beanie[0].' inséré à avec l\'id '. $id;
+    echo 'Erreur lors de l\'insertion de la donnée : '.$beanie[0].' inséré à avec l\'id '. $id;
 }
