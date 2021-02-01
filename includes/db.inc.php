19fc4f2488a6e7cbfd3700fac808e5607a4e852f
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-02-01 13:25:52 +0100

    alice1-rectif

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -4,4 +4,4 @@ $dbName = 'beanies_exam';
 $dbUser = 'root';
 $dbPass = 'root'; // root si vous utilisez un Mac avec MAMP
 $dbHost = 'localhost'; // ou 127.0.0.1, en cas de soucis (mais les deux devraient être identiques)
-$dbDsn = 'mysql:dbname='.$dbName.';host='.$dbHost;
+$dbDsn = 'mysql:host='.$dbHost.';dbname='.$dbName;
