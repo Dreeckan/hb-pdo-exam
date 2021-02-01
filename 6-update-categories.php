5e562626929f6101898ed59f8eeb2fcd84859a47
Adam FARES <dadi71410@outlook.fr>
2021-02-01 17:20:50 +0100

    6. Mise à jour de la table de liaison (product_has_category)

--- 6-update-categories.php
+++ 6-update-categories.php
@@ -1,3 +1,7 @@
 <?php
 
 include 'includes/connect.php';
+
+$sql = "SELECT * FROM product LEFT JOIN product_has_category ON product_has_category.id_product = product.id WHERE product_has_category.id_category IS NULL";
+$beanie = $connection->query($sql);
+
