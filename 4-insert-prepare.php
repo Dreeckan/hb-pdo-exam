e0b825d177c7f3b42ea29b71f95c584cd07dce33
franckcode6 <franck.quasevi@yahoo.com>
2021-02-01 15:01:21 +0100

    Partie 4

--- 4-insert-prepare.php
+++ 4-insert-prepare.php
@@ -9,3 +9,19 @@ $data = [
     'Cache-oreilles',
     'Bandeaux',
 ];
+
+$sql = "INSERT INTO `category`(`name`) VALUES (:name)";
+foreach ($data as $beanie) {
+    $name = $beanie;
+    $statement = $connection->prepare($sql);
+    $statement->bindParam(':name', $name, PDO::PARAM_STR);
+    $isDone = $statement->execute();
+
+    if (!$isDone) {
+        throw new Exception('Erreur');
+    }
+        
+    $id = $connection->lastInsertId();
+        
+    echo 'Erreur! '.$beanie[0].' inséré à avec l\'id '. $id;
+}
