27b5a21b2a59a8c489248476de852cb2fa853119
Adam FARES <dadi71410@outlook.fr>
2021-02-01 16:31:53 +0100

    5. Exécution d'une requête préparée (SELECT)

--- 5-select-prepare.php
+++ 5-select-prepare.php
@@ -2,7 +2,19 @@
 
 include 'includes/connect.php';
 
-$data = [];
+$sql = "SELECT product.name'name', description, price, category.name'categorie', stock FROM product 
+        LEFT JOIN product_has_category ON product.id = product_has_category.id_product
+        LEFT JOIN category ON category.id = product_has_category.id_category";
+$statement = $connection->prepare($sql);
+$countAffected = $statement->execute();
+
+if (!$countAffected) {
+        throw new Exception('Erreur');
+}
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+$results = $statement->fetchAll();
+
+$data = $results;
 
 ?>
 
@@ -14,3 +26,28 @@ $data = [];
         <th>Catégories</th>
         <th>En stock</th>
     </tr>
+
+<?php foreach ($data as $beanie) {
+    echo '
+        <tr>
+            <td>
+                '.$beanie['name'].'
+            </td>   
+            <td>
+                '.$beanie['description'].'
+            </td>      
+            <td>
+                '.$beanie['price'].'
+            </td>                        
+            <td>
+                '.$beanie['categorie'].'
+            </td>
+            <td>
+                '.$beanie['stock'].'
+            </td>
+        </tr>';
+} 
+
+?>
+
+</table>
