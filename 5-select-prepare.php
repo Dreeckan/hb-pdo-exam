e57705b8cc823360326510c44136af476cfa3d3a
Elwilya <69197349+Elwilya@users.noreply.github.com>
2021-02-01 16:50:13 +0100

    Exercice 5 : Select prepare

--- 5-select-prepare.php
+++ 5-select-prepare.php
@@ -3,6 +3,17 @@
 include 'includes/connect.php';
 
 $data = [];
+$sql = "SELECT product.name as name, description, price, category.name as category, stock
+FROM product
+        LEFT JOIN product_has_category ON product.id = product_has_category.id_product
+        LEFT JOIN category ON category.id = product_has_category.id_category";
+
+
+
+$statement = $connection->prepare($sql);
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+$data = $results;
 ?>
 
 <table>
@@ -13,13 +24,14 @@ $data = [];
         <th>Catégories</th>
         <th>En stock</th>
     </tr>
-    <?php foreach ($data as $beanie) { ?>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-        </tr>
-    <?php } ?>
-</table>
+    <?php foreach ($data as $beanie) {
+    echo '
+                <tr>
+                    <td>' .$beanie['name']. '</td>
+                    <td>' .$beanie['description']. '</td>
+                    <td>' .$beanie['price']. '</td>
+                    <td>' .$beanie['category']. '</td>
+                    <td>' .$beanie['stock']. '</td>
+                </tr>';
+} ?>
+    </table>
