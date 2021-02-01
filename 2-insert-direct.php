d99272c425a5ed9caa1e5ae9a510d7f2d267cb85
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-02-01 15:46:07 +0100

    Commit de l'exo 2 qui fonctionne :)

--- 2-insert-direct.php
+++ 2-insert-direct.php
@@ -45,14 +45,14 @@ Matière: 32 % Laine, 32 % Polyamide, 30 % Viscose, 3 % Cashmere 3 % Autres fibr
         'stock'       => 1,
     ],
 ];
-
- $sql = "INSERT INTO product(`id`, `name`, `description`, `updated_at`, `price`, `stock`) VALUES ( ' ', 'Bonnet - Oscar light grey', 'Voici un bonnet urbain par excellence, avec sa forme allongée moderne et sa matière très douce en coton biologique que vous apprécierez tout au long de l’année. Il est tricoté en Suède dans les ateliers de la marque traditionnelle Sätila.
-Matière: 100 % Coton Biologique', NOW(), '31.43', 1), ( ' ', 'Bonnet - 'Headsock in recycled yarn black', Affichez une allure originale avec ce sympathique bonnet long noir, qui tient bien en place grâce à ses côtes verticales. Il est composé d’un mélange très doux de fibres provenant en partie de bouteilles en plastique recyclées.
-Matière: 50 % Acrylique, 50 % Polyester', NOW(), '25.90', 1), ( ' ', 'Bonnet - Rada camel', 'Chez la marque suédoise Sätila, l’environnement et la qualité ne sont pas pris à la légère, notamment avec ce magnifique bonnet long tricoté en Suède avec 41 % de fibres recyclées, combinant la laine, la viscose et le cachemire pour un résultat doux et bien chaud.
-Matière: 32 % Laine, 32 % Polyamide, 30 % Viscose, 3 % Cashmere 3 % Autres fibres'
-, NOW(), '34.23', 1)  ";
-
-$stmt = $connection->exec($sql);
-if ($stmt === false) {
-    exit("Erreur lors de l'insertion de la donnée :");
+foreach ($data as $beanie) {
+    $name = $beanie['name'];
+    $description = $beanie['description'];
+    $price = $beanie['price'];
+    $stock = $beanie['stock'];
+    $sql = "INSERT INTO product(`name`, `description`, `updated_at`, `price`, `stock`) VALUES ('$name', '$description', NOW(), '$price', '$stock' ) ";
+    $stmt = $connection->exec($sql);
+    if ($stmt === false) {
+        exit("Erreur lors de l'insertion de la donnée :");
+    }
 }
