f9b5550caf9472c980ffff75dfde5fea70236267
Dekleor <donier.matteo@gmail.com>
2021-02-01 14:46:22 +0100

    5-select-prepare

--- 5-select-prepare.php
+++ 5-select-prepare.php
@@ -3,23 +3,41 @@
 include 'includes/connect.php';
 
 $data = [];
+
+$sql = "SELECT * FROM product";
+$statement = $connection->prepare($sql);
+$isDone = $statement->execute();
+
+if (!$isDone) {
+    throw new Exception('Erreur');
+}
+$statement->setFetchMode(PDO::FETCH_ASSOC);
+$data = $statement->fetchAll();
 ?>
 
 <table>
     <tr>
+        <th>Categorie</th>
         <th>Nom</th>
         <th>Description</th>
         <th>Prix</th>
-        <th>Catégories</th>
         <th>En stock</th>
     </tr>
     <?php foreach ($data as $beanie) { ?>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td> <?php if(!empty($beanie['categorie'])) {
+                echo empty($beanie['categorie']);
+                } else {
+                echo 'non défini';
+                }?></td> 
+            <td> <?php echo $beanie['name']; ?></td>;
+            <td> <?php echo $beanie['description']; ?></td>;
+            <td> <?php echo $beanie['price']; ?> €</td>;
+            <td> <?php if($beanie['stock'] == 1) {
+                echo 'oui';
+                } else {
+                echo 'non';
+                }?></td>
         </tr>
     <?php } ?>
 </table>
