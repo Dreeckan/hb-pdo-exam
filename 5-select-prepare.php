156049d313a1f428aaa72b1284b9c5f157815a2b
franckcode6 <franck.quasevi@yahoo.com>
2021-02-01 15:42:22 +0100

    Partie 5

--- 5-select-prepare.php
+++ 5-select-prepare.php
@@ -13,13 +13,22 @@ $data = [];
         <th>Catégories</th>
         <th>En stock</th>
     </tr>
-    <?php foreach ($data as $beanie) { ?>
+    <?php
+    $sql = "SELECT * FROM product JOIN product_has_category on product_has_category.id_product = product.id JOIN category on category.id = product_has_category.id_category ";
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+     foreach ($data as $beanie) {
+         var_dump($data)
+         ?>
         <tr>
+            <td><?= $beanie['name'] ?></td>
+            <td><?= $beanie['description'] ?></td>
+            <td><?= $beanie['price'] ?></td>
             <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
+            <td><?= $beanie['stock'] ?></td>
         </tr>
-    <?php } ?>
+    <?php
+     } ?>
 </table>
