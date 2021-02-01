0b616ccda46fad9cff9e8f1315c09565f7cc48e7
franckcode6 <franck.quasevi@yahoo.com>
2021-02-01 14:30:02 +0100

    Partie 2

--- 2-insert-direct.php
+++ 2-insert-direct.php
@@ -45,3 +45,8 @@ Matière: 32 % Laine, 32 % Polyamide, 30 % Viscose, 3 % Cashmere 3 % Autres fibr
         'stock'       => 1,
     ],
 ];
+
+foreach ($data as $beanie) {
+    $sql = "INSERT INTO `product`(`name`, `description`, `updated_at`, `price`, `stock`) VALUES ('$beanie[name]', '$beanie[description]', 'NOW()', '$beanie[price]', '$beanie[stock]')";
+    $count = $connection->exec($sql);
+}
