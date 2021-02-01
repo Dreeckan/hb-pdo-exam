a1289915e19f811a13f7925257c9ec3405882f49
franckcode6 <franck.quasevi@yahoo.com>
2021-02-01 16:45:36 +0100

    Partie 6

--- 6-update-categories.php
+++ 6-update-categories.php
@@ -7,4 +7,12 @@ FROM product LEFT JOIN product_has_category ON product.id = product_has_category
     $statement = $connection->query($sql);
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    var_dump($results);
+$sql = "SELECT * FROM category";
+    $statement = $connection->query($sql);
+    $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+ var_dump($results);
+var_dump($data);
+
+$sql = "INSERT IGNORE INTO `product_has_category`(`id_category`, `id_product`) VALUES (3, 0), (4, 1), (5, 2), (6, 3), (6, 4), (6, 5), (6, 6), (5, 7)";
+    $count = $connection->exec($sql);
