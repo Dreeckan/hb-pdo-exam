002c15532fca2a5b70d0ade79fd85b255c840f55
geoffrey <biggeo22@hotmail.fr>
2021-02-01 18:41:19 +0100

    Exercice 4

--- 4-insert-prepare.php
+++ 4-insert-prepare.php
@@ -2,6 +2,8 @@
 
 include 'includes/connect.php';
 
+
+
 $data = [
     'Chapeaux',
     'Bérets',
@@ -9,3 +11,18 @@ $data = [
     'Cache-oreilles',
     'Bandeaux',
 ];
+
+$sql = "INSERT INTO category(Bonnets, Casquette) VALUES (:bonnets, :casquette)";
+$stmt = $connection->prepare($sql);
+
+foreach ($data as $beanie) {
+    $stmt->bindParam(':Bonnets', $data[0], PDO::PARAM_STR);
+    $stmt->bindParam(':Casquette', $data[1], PDO::PARAM_STR);
+
+    var_dump($data);
+    $ret = $stmt->execute();
+
+    if ($ret === false) {
+        throw new Exception('Erreur lors de l\'insertion de la donnée' . $data[0]);
+    }
+}
