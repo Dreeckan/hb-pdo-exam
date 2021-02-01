0844257e33ed0aac0678f2127e721dd0af4aac1c
Alice1360 <72809385+Alice1360@users.noreply.github.com>
2021-02-01 14:44:52 +0100

    commit4

--- 4-insert-prepare.php
+++ 4-insert-prepare.php
@@ -9,3 +9,11 @@ $data = [
     'Cache-oreilles',
     'Bandeaux',
 ];
+
+$sql = "INSERT INTO category (`name`) VALUES (:name) ";
+$stmt = $connection->prepare($sql);
+
+foreach ($data as $info) {
+    $stmt->bindParam(':name', $info, PDO::PARAM_STR);
+    $ret = $stmt->execute();
+}
